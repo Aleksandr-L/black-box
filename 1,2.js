3f4b941bddac9d3fe45ae2095078266d33bbd6d9
Aleksandr-L <aleksandr_leo@mail.ru>
2019-09-26 13:23:27 +0300

    26.09

--- 1,2.js
+++ 1,2.js
@@ -1,54 +1,83 @@
 function plus(event) {
-    event.preventDefault()
-    // var num1 = document.getElementById('n1').value;
-    // var num2 = document.getElementById('n2').value;
-    //     num2 = parseInt(num2);
-    //     num1 = parseInt(num1);
-    // var result = num1 + num2;
-    //     document.getElementById('out').innerHTML = result;
+     event.preventDefault();
+   //  var num1 = document.getElementById('n1').value;
+   //  var num2 = document.getElementById('n2').value;
+   //      num2 = parseInt(num2);
+   //      num1 = parseInt(num1);
+   //  var result = num1 + num2;
+   //      document.getElementById('out').innerHTML = result;
 //шаг 1. Отправка  на сервер к файлу calc.php
-
-    fetch('/calc.php', {
+//
+    fetch("calc.php", {
         method: 'POST',
         body: new FormData(document.forms[0])
-        })
+    })
         .then(response => response.text())
         .then(result => {
-         //Шаг 3. Получение ответа от сервира, показ пользователю
+            //Шаг 3. Получение ответа от сервира, показ пользователю
          document.getElementById('out').innerHTML = result;
-        })
+        });
 
-}
-function btnminus(){
-    var num1,num2,result;
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    num1 = parseInt(num1);
-    result = num1 - num2;
-    document.getElementById('out').innerHTML = result;
-}
-function btnmult(){
-    var num1,num2,result;
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    num1 = parseInt(num1);
-    result = num1 * num2;
-    document.getElementById('out').innerHTML = result;
-}
-function btndel() {
-    var num1, num2, result;
-    num1 = document.getElementById('n1').value;
-    num2 = document.getElementById('n2').value;
-    num2 = parseInt(num2);
-    num1 = parseInt(num1);
-    result = num1 / num2;
-    document.getElementById('out').innerHTML = result;
-}
-function f() {
-    var nd = document.getElementsByTagName("div").value
-
-}
+ };
 
 
+
+// function btnminus(event){
+//     event.preventDefault();
+//     // var num1,num2,result;
+//     // num1 = document.getElementById('n1').value;
+//     // num2 = document.getElementById('n2').value;
+//     // num2 = parseInt(num2);
+//     // num1 = parseInt(num1);
+//     // result = num1 - num2;
+//     fetch("/calc.php", {
+//         method: 'POST',
+//         body: new FormData(document.forms[0])
+//     })
+//         .then(response => response.text())
+//         .then(result => {
+//             //Шаг 3. Получение ответа от сервира, показ пользователю
+//             document.getElementById('out').innerHTML = result;
+//         })
+// }
+// function btnmult(event){
+//     event.preventDefault();
+//     // var num1,num2,result;
+//     // num1 = document.getElementById('n1').value;
+//     // num2 = document.getElementById('n2').value;
+//     // num2 = parseInt(num2);
+//     // num1 = parseInt(num1);
+//     // result = num1 * num2;
+//     fetch("/calc.php", {
+//         method: 'POST',
+//         body: new FormData(document.forms[0])
+//     })
+//         .then(response => response.text())
+//         .then(result => {
+//             //Шаг 3. Получение ответа от сервира, показ пользователю
+//             document.getElementById('out').innerHTML = result;
+//         })
+//
+// }
+// function btndel(event) {
+//     event.preventDefault();
+//     // var num1, num2, result;
+//     // num1 = document.getElementById('n1').value;
+//     // num2 = document.getElementById('n2').value;
+//     // num2 = parseInt(num2);
+//     // num1 = parseInt(num1);
+//     // result = num1 / num2;
+//     fetch("/calc.php", {
+//         method: 'POST',
+//         body: new FormData(document.forms[0])
+//     })
+//         .then(response => response.text())
+//         .then(result => {
+//             //Шаг 3. Получение ответа от сервира, показ пользователю
+//             document.getElementById('out').innerHTML = result;
+//         })
+// }
+// function f() {
+//     var nd = document.getElementsByTagName("div").value
+//
+// };
