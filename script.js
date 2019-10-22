a7ba68ec3e34e50c03120eb5c93b215aea5a4b37
Aleksandr-L <aleksandr_leo@mail.ru>
2019-10-22 12:08:45 +0300

    22.10

--- script.js
+++ script.js
@@ -1,82 +1,78 @@
-    /*var link = document.createElement("link");
-    document.head.appendChild(link);
-    document.querySelector("links");
-    document.links.rel = "stylesheet";
-    document.links.href="style.css";
-    document.links.type="text/css";*/
 
-    var style = document.createElement('link');
+    let style = document.createElement('link');//присоединение файла
     document.head.appendChild(style);
+    document.querySelector("link");
     style.rel   = 'stylesheet';
     style.type  = 'text/css';
     style.href  = "style.css";
 
-    var center = document.createElement("center");
-    document.body.appendChild(center);
-    document.querySelector("center");
-
-    var form = document.createElement("form");
+    let form = document.createElement("form");//форма
     document.body.appendChild(form);
-    document.querySelector("form");
+    form.className = "form1";
+    form.action = "";
+    form.method = "POST";
 
-    var pole1 = document.createElement("input");
+    let div1 = document.createElement("div");//для чего див
+    document.body.appendChild(div1);
+    document.body.div = "Super notebook";
+    div1.innerHTML = "Super notebook";
+    div1.className = "header";
+    document.querySelector(".form1").append(div1);
+
+    let spaceL = document.createElement("br");     //пробелы заменить в Css
+    document.body.appendChild(spaceL);
+    document.querySelector(".form1").append(spaceL);
+
+    let pole1 = document.createElement("input");  //поле ввода №1
     document.body.appendChild(pole1);
     pole1.name = "login";
     pole1.placeholder ="имя пользователя ";
-    pole1.class = "pass";
+    pole1.className = "pass";
+    document.querySelector(".form1").append(pole1);
 
-    var space = document.createElement("br");
-    document.body.appendChild(space);
-    var space = document.createElement("br");
-    document.body.appendChild(space);
+    let spacel = document.createElement("br");
+    document.body.appendChild(spacel);
+    document.querySelector(".form1").append(spacel);
 
-    var pole2 = document.createElement("input");
+    let pole2 = document.createElement("input");  //поле ввода №2
     document.body.appendChild(pole2);
     pole2.name = "password";
     pole2.placeholder ="пароль";
-    pole2.class = "pass";
+    pole2.className = "pass";
+    document.querySelector(".form1").append(pole2);
 
-    var space = document.createElement("br");
-    document.body.appendChild(space);
-    var space = document.createElement("br");
-    document.body.appendChild(space);
+    let space1 = document.createElement("br");
+    document.body.appendChild(space1);
+    document.querySelector(".form1").append(space1);
 
-    var pole3 = document.createElement("input");
+    let pole3 = document.createElement("input");  //поле ввода №3
     document.body.appendChild(pole3);
     pole3.name = "confirm";
     pole3.placeholder ="подтвердите пароль";
-    pole3.class = "pass";
+    pole3.className = "pass";
+    document.querySelector(".form1").append(pole3);
 
-    var space = document.createElement("br");
-    document.body.appendChild(space);
-    var space = document.createElement("br");
-    document.body.appendChild(space);
+    let space2 = document.createElement("br");
+    document.body.appendChild(space2);
+    document.querySelector(".form1").append(space2);
 
-    var pole4 = document.createElement("input");
+    let pole4 = document.createElement("input");   //поле ввода №4
     document.body.appendChild(pole4);
     pole4.name = "mail";
     pole4.placeholder ="email";
-    pole4.class = "pass";
+    pole4.className = "pass";
+    document.querySelector(".form1").append(pole4);
 
-    var space = document.createElement("br");
-    document.body.appendChild(space);
-    var space = document.createElement("br");
-    document.body.appendChild(space);
+    let space3 = document.createElement("br");
+    document.body.appendChild(space3);
+    document.querySelector(".form1").append(space3);
 
-    var buttonRegistration = document.createElement("button");
+    let buttonRegistration = document.createElement("button"); //поле кнопки
     document.body.appendChild(buttonRegistration);
-    buttonRegistration.name = "registration";
+    buttonRegistration.name = "submit";
     buttonRegistration.placeholder ="регистрация";
-    buttonRegistration.class = "registration";
+    buttonRegistration.className= "registration";
+    buttonRegistration.textContent = "Register";
+    document.querySelector(".form1").append(buttonRegistration);
+    buttonRegistration.type = "submit";
 
-
-    // let buttonRegistration = document.querySelector( ".registration");
-    // let inputIn = document.querySelector( ".pass");
-    // // //inputIn.value = "hi";
-    // inputIn.innerHTML;
-    // buttonRegistration.onclick = function() {
-    // console.log(inputIn.value);
-    // }// кнопка регистрации
-    /*for (let node of document.body.childNodes) {
-        alert(node); // покажет все узлы из коллекции
-    }*/
