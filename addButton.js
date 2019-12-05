962a08262ec55e8e80a32e15e0e6cfe5cdd7bedc
Aleksandr-L <aleksandr_leo@mail.ru>
2019-12-05 21:11:38 +0300

    проверка fetch 05.12.19

--- addButton.js
+++ addButton.js
@@ -1,28 +1,64 @@
 let a      = document.querySelector('.search');
 let button = document.querySelector('.note');
+let button_save = document.querySelector('.save');
 let div    = document.querySelector(".block-left");
 
 
-button.onclick = function AddNewNote () {
-let button_two = document.createElement("div");
-div.appendChild(button_two);
-document.querySelector(".note");
-button_two.innerHTML = a.value;
-button_two.className = "class_1 ";
-let space = document.createElement("br");
-div.appendChild(space);
-};
+function chpok(){
 
+}
 
-function note(event) {
-        event.preventDefault();
+button_save.onclick = function (event) {
+    //event.preventDefault();
     let request =  new FormData(document.forms[0]);
     fetch("planner.php", {
         method: 'POST',
         body: request
     })
         .then(response => response.text())
-        .then(result => {"planner_Get.php"
-        });
-    }
+        .then(result =>chpok())};
 
+
+
+function registr(event) {
+ //        event.preventDefault();
+    let request =  new FormData(document.forms[0]);
+    fetch("planner_Get.php", {
+        method: 'POST',
+        body: request;
+    })
+        .then(response => response.text())
+        .then(result => {
+ //            if (result == "" ) {
+ //                window.location.href = "login.php";
+ //            }
+ //            else { document.body.append(result)};
+ //        });
+ //    }
+
+
+// function add_title(data) {
+//     let button_two = document.createElement("div");
+//     div.appendChild(button_two);
+//     document.querySelector(".note");
+//     button_two.innerHTML = data;
+//     button_two.className = "class_1 ";
+//     let space = document.createElement("br");
+//     div.appendChild(space);
+// };
+//
+//     //add_title(request.get("users2"));
+// }
+//
+// function heandler(data) {
+//    // let b = JSON.parse(data);
+//     let c = "sdf";
+// }
+//
+//    fetch("planner_Get.php", {
+//         method: 'POST'
+//     }).then(response => response.json())
+//        .then(result =>heandler(result)
+//            //document.getElementById("data").innerHTML = result
+//        )
+//
--- planner_Get.php
+++ planner_Get.php
@@ -4,11 +4,15 @@ $link  = mysqli_connect("localhost", "root", "", "notebook") or die(mysqli_error
 $query = mysqli_query($link, "SELECT * FROM `daily_planner`") or die(mysqli_error());
     //mysqli_select_db($query, $link);
 
+$arr = [];
 while ($row = mysqli_fetch_array($query, MYSQL_BOTH)) {
-          echo "<hr><br> $row[id]";
-          echo "<br> $row[users]";
+         // echo "<hr><br> $row[id]";
+          //echo "<br> $row[users]";
+          $arr[] = ["id" => $row["id"], "user" => $row["users"]];
     }
     //$row = mysqli_fetch_array($query, MYSQL_BOTH);
     //echo '<pre>' . print_r($row, 1) . '</pre>';
 mysqli_close($link);
+
+echo json_encode($arr);
 ?>
